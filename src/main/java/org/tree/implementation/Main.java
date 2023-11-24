9bf97fb86dcfce1a4bc43fdfd98dc8bc360ff0e0
andrade-filipe <filipeandrade.work@gmail.com>
2023-11-24 04:05:15 -0300

    responses are on point

--- src/main/java/org/tree/implementation/Main.java
+++ src/main/java/org/tree/implementation/Main.java
@@ -2,21 +2,30 @@ package org.tree.implementation;
 
 import org.tree.implementation.adapter.Tree;
 import org.tree.implementation.avl.AVL;
+import org.tree.implementation.bst.BinarySearchTree;
 import org.tree.implementation.redblack.RedBlack;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ÁRVORE RED-BLACK");
-        var redBlackTree = getRedBlackTree();
+        System.out.println("ÁRVORE BST NORMAL");
+        var bst = getBinarySearchTree();
 
-        redBlackTree.printTree();
-        redBlackTree.traverse();
+        bst.printTree();
+        bst.traverse();
 
+        System.out.println("****************************");
         System.out.println("ÁRVORE AVL");
         var avl = getAvlTree();
 
         avl.printTree();
         avl.traverse();
+
+        System.out.println("****************************");
+        System.out.println("ÁRVORE RED-BLACK");
+        var redBlackTree = getRedBlackTree();
+
+        redBlackTree.printTree();
+        redBlackTree.traverse();
     }
 
     private static Tree<String> getAvlTree() {
@@ -57,5 +66,26 @@ public class Main {
             .insert("M")
             .insert("N");
         return redBlackTree;
+    }
+
+    private static Tree<String> getBinarySearchTree() {
+        Tree<String> binarySearchTree = new BinarySearchTree<>();
+
+        binarySearchTree
+            .insert("A")
+            .insert("B")
+            .insert("C")
+            .insert("D")
+            .insert("E")
+            .insert("F")
+            .insert("G")
+            .insert("H")
+            .insert("I")
+            .insert("J")
+            .insert("K")
+            .insert("L")
+            .insert("M")
+            .insert("N");
+        return binarySearchTree;
     }
 }
